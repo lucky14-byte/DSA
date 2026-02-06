97b66993210ca050b528b8a0dff64a0de6c51cd9
lucky14-byte <lucky21181700@gmail.com>
2026-02-06 15:24:13 +0530

    using vector fn

--- vec fn.cpp
+++ vec fn.cpp
@@ -5,8 +5,15 @@ using namespace std;
 int main() {
     vector<int>vec ;
     cout<<"size = "<<vec.size()<<endl; // for size
-    vec.push_back(25);
+    vec.push_back(25); //for add the element at the end
+    vec.push_back(35);
+    vec.push_back(45);
     cout<<"after puch back size = "<<vec.size()<<endl; // for size
+
+    vec.pop_back(); // for remove the last element
+    cout<<vec.front()<<endl; // for first element
+    cout<<vec.back()<<endl; // for last element
+    cout<<vec.at(0)<<endl; // for element at index 0
 
     for(int val : vec) {
         cout<<val<<endl;
